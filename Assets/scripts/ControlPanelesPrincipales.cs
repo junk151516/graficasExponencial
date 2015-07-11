45164b28dadffb8d27965cf04dbe18119d0c83f7
junk151516 <junk151516@gmail.com>
2015-07-11 16:46:40 -0500

    sabado

---
+++ Assets/scripts/ControlPanelesPrincipales.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlPanelesPrincipales : MonoBehaviour {
+
+	public GameObject panelCreditos;
+	public GameObject panelInicial;
+	public GameObject panelTutorial1;
+	public GameObject panelTutorial2;
+	public GameObject panelTutorial3;
+	public GameObject panelSplash;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ocultarPanel(int pID){
+		
+		if (pID == 0) { //Ocultar Inicio
+			panelInicial.SetActive(false);
+		}
+		if (pID == 1) { //Mostrar Creditos
+			panelInicial.SetActive(false);
+			panelCreditos.SetActive(true);
+		}
+
+		if (pID == 2) { //Ocultar Creditos
+			panelInicial.SetActive(true);
+			panelCreditos.SetActive(false);
+		}
+
+		if (pID == 3) {
+			panelSplash.SetActive(false);
+
+			/*if(panelTutorial1.SetActive(true)==true){
+				panelTutorial1.SetActive(false);
+				panelTutorial2.SetActive(true);
+			}*/
+		}
+		if (pID == 4) {
+			panelTutorial2.SetActive(true);
+		}
+		if (pID == 5) {
+			panelTutorial3.SetActive(true);
+		}
+
+
+	}
+}
